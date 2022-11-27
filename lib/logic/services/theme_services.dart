dea0d0713f9025cb0a2a02e06fbd67edb5cbc576
3mr M7mud <amrosy00@gmail.com>
2022-11-27 17:16:59 +0200

    Theme logic

---
+++ lib/logic/services/theme_services.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+class ThemeController {
+  final GetStorage _box = GetStorage();
+  final _key = 'isDarkMode';
+
+  ThemeMode get theme => _loadThemeFromBox() ? ThemeMode.dark : ThemeMode.light;
+
+  bool _loadThemeFromBox() {
+    return _box.read(_key) ?? false;
+  }
+
+  _saveThemeToBox(bool isDarkMode) {
+    _box.write(_key, isDarkMode);
+  }
+
+  void switchTheme() {
+    Get.changeThemeMode(_loadThemeFromBox() ? ThemeMode.light : ThemeMode.dark);
+    _saveThemeToBox(!_loadThemeFromBox());
+  }
+}
